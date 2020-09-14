bef653d4c7581f68f5be8554b73e0f3812a85c93
KOSTA <KOSTA@192.168.0.168>
2020-09-14 15:59:56 +0900

    현민 량훈 수정

--- src/VIew/hyunmin.java
+++ src/VIew/hyunmin.java
@@ -2,7 +2,7 @@ package VIew;
 
 public class hyunmin {
 	public void test() {
-		System.out.println("Çö¹ÎÀÌdd HI HELLO");
+		System.out.println("Çö¹ÎÀÌ HELLO");
 		System.out.println("**************");
 	}
 
